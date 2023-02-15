d6d35b80118fac54b41888b310fdb13592dff4a9
chenrenliang <474571921@qq.com>
2023-02-15 15:01:07 +0000

    Initial commit

---
+++ src/components/User/UserPicker.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { users } from "../../static.json";
+import Spinner from "../UI/Spinner";
+
+export default function UserPicker() {
+  const [user, setUsers] = useState(null);
+
+  useEffect(() => {
+    fetch("https://hhc4vw-3001.preview.csb.app/users")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data", data);
+        setUsers(data);
+      });
+  }, []);
+
+  if (user === null) {
+    return <Spinner />;
+  }
+  return (
+    <select>
+      {users.map((u) => (
+        <option key={u.id}>{u.name}</option>
+      ))}
+    </select>
+  );
+}
